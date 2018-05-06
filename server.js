24b40488a52c07f57d979dd4a8c7aff0a0e05b2c
thanhvc <vucongthanh92@gmail.com>
2018-05-06 14:12:17 +0700

    push

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("The server is running...");
+var http = require('http');
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {
+		"Context-type" : "text/plain"
+	});
+
+	response.write("Your url is: " + resquest.url);
+	response.end();
+});
+
+server.listen(3000, function() {
+	console.log("Connected Successfull");
+});
